66f25f2795c223f7738a82a760a44f74c138afd3
pforbrig <pedroforbrig@gmail.com>
2021-10-04 18:30:26 -0300

    UserMenu componentizado

---
+++ src/components/UserMenu/index.js
@@ -0,0 +1,45 @@
+import './style.css';
+import Usuario from '../../images/usuario.svg';
+import Logout from '../../images/log-out.svg';
+import EditIcon from '../../images/edit-icon.svg';
+import { useState, useContext} from 'react';
+import { Popover } from '@mui/material';
+import { AuthContext } from '../../routes.js';
+
+
+
+export default function UserMenu(){
+    const [openUserMenu, setOpenUserMenu] = useState(false);
+    const { logOut } = useContext(AuthContext);
+
+    function handleCloseUserMenu () {
+        setOpenUserMenu (false);
+      }
+
+    return(
+        <>
+        <img src={Usuario}
+        alt='usuario-menu'
+        id='user-menu'
+        className='icon-usuario' 
+        onClick={() => setOpenUserMenu(true)} />
+        <Popover
+          id='menu-user'
+          open={openUserMenu}
+          onClose={handleCloseUserMenu}
+          anchorReference='anchorPosition'
+          anchorPosition={{ top: 80, left: 1300 }}>
+            <div className='flex-column user-menu'>
+              <div className='row-menu'>
+                <img src={EditIcon} alt="edit" />
+                <button className='btn-usermenu'>Editar</button>
+              </div>
+              <div className='row-menu' onClick={() => logOut()}>
+                <img src={Logout} alt="logout" />
+                <button className='btn-usermenu'>Deslogar</button>
+              </div>
+            </div>
+        </Popover>
+        </>
+    )
+}
