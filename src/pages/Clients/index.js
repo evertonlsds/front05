88a029fe6430ce9e32fec850b152d77f41f35cf4
pforbrig <pedroforbrig@gmail.com>
2021-10-06 11:58:04 -0300

    Populando cards de clientes

--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -3,9 +3,35 @@ import SideBar from '../../components/SideBar';
 import UserMenu from '../../components/UserMenu';
 import ClientCard from '../../components/ClientCard';
 import { useHistory } from 'react-router';
+import { useState, useContext, useEffect } from 'react';
+import { AuthContext } from '../../routes.js';
 
 export default function Clients() {
     const history = useHistory();
+    const [clients, setClients] = useState([]);
+    const { token } = useContext(AuthContext);
+
+    async function getClients() {
+
+        const response = await fetch("https://api-desafio-05.herokuapp.com/clientes", {
+            method: 'GET',
+            headers: {
+                'Content-Type': "application/json",
+                "charset": "utf-8",
+                'Authorization': `Bearer ${token} `
+            },
+        });
+
+        const resposta = await response.json();
+
+        setClients(resposta);
+        console.log(resposta)
+    }
+
+    useEffect(() => {
+        getClients();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <div className='flex-row'>
@@ -22,8 +48,7 @@ export default function Clients() {
                         <p className='legends'> Cobranças Recebidas</p>
                         <p className='legends'> Status</p>
                     </div>
-                    <ClientCard />
-                    <ClientCard />
+                    <ClientCard clients={clients} />
                 </div>
             </div>
         </div>
