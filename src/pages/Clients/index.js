b89d110c71c7075b98bad2bf90ebbc108191e6a9
pforbrig <pedroforbrig@gmail.com>
2021-10-05 22:13:37 -0300

    Criação do componente de ClientCard e melhorias na página de clients

--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import SideBar from '../../components/SideBar';
 import UserMenu from '../../components/UserMenu';
+import ClientCard from '../../components/ClientCard';
 import { useHistory } from 'react-router';
 
 export default function Clients() {
@@ -9,9 +10,21 @@ export default function Clients() {
     return (
         <div className='flex-row'>
             <SideBar />
-            <UserMenu />
-            <div>
-                <button className="btn-white-pink" onClick={() => history.push('/newclient')}>Adicionar Cliente</button>
+            <div className='main-clients'>
+                <UserMenu />
+                <div>
+                    <button className="btn-white-pink" onClick={() => history.push('/newclient')}>Adicionar Cliente</button>
+                </div>
+                <div className='cards-container'>
+                    <div className='container-legends'>
+                        <p className='legends' style={{ 'padding-right': '100px' }}> Cliente</p>
+                        <p className='legends'> Cobranças Feitas</p>
+                        <p className='legends'> Cobranças Recebidas</p>
+                        <p className='legends'> Status</p>
+                    </div>
+                    <ClientCard />
+                    <ClientCard />
+                </div>
             </div>
         </div>
     )
