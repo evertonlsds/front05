d13ceffd746b736d3a7fc0c6788d3c7b41997ac8
pforbrig <pedroforbrig@gmail.com>
2021-10-08 22:31:05 -0300

    Novo datepicker

---
+++ src/components/CustomDatePicker/index.js
@@ -0,0 +1,36 @@
+import './styles.css';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale } from "react-datepicker";
+import pt from 'date-fns/locale/pt';
+import { forwardRef } from 'react'
+import Calendar from '../../images/calendar.svg'
+import { Controller } from 'react-hook-form'
+
+
+registerLocale('pt', pt)
+
+
+export default function CustomDatePicker(props) {
+    const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+        <button className="datepicker" onClick={onClick} ref={ref}>
+            {value}
+            <img src={Calendar} alt='calendar' />
+        </button>));
+
+    return (
+        <Controller
+            name='vencimento'
+            control={props.control}
+            render={({ field: { onChange, onBlur, value, ref } }) => (<DatePicker
+                dateFormat="dd MMMM 'de' yyyy"
+                selected={value}
+                value={value}
+                locale={pt}
+                onChange={onChange}
+                popperPlacement="top-start"
+                customInput={<ExampleCustomInput />}
+            />)
+            } />
+    );
+};
